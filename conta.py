cb6402777a7d1791f016a9c7c0fcb5ff5c679f68
ericovinicosta <elcosta@gmail.com>
2023-02-09 21:14:13 -0300

    cliente e datas

--- conta.py
+++ conta.py
@@ -1,18 +1,34 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print('contruindo o objeto {}'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def saldo(self):
-        print('Seu saldo é {}'.format(self.saldo))
+        print('Seu saldo é {}'.format(self.__saldo))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
+    def transfere(self, conta, valor):
+        self.saca(valor)
+        conta.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
     
+    def get_titular(self):
+        return self.__titular
+    
+    def get_limite(self):
+        return self.__limite
+    
+    def set_limite(self, limite):
+        self.__limite = limite
+
+        
